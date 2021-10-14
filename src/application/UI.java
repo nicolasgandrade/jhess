e00d6beb95a0940db7a755a20a1f5ee1e40089bb
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-10-14 20:52:13 -0300

    feat: moving pieces (with no chess rules yet)

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,11 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -29,6 +33,17 @@ public class UI {
 
     //---------------------------------------------------------------
 
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Error: Valid values are from a1 to h8.");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
 
