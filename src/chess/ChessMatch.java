4cd9a4f9663b256616bef459ea90840b719a3f85
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-10-15 19:50:34 -0300

    feat: now the king can move

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -66,6 +66,9 @@ public class ChessMatch {
         placeNewPiece('b', 1, new King(board, Color.WHITE));
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('f', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Rook(board, Color.WHITE));
         placeNewPiece('g', 1, new Rook(board, Color.WHITE));
